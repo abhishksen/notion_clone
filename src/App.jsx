f8b636246762690446db82a250a4497f4c20457f
Abhishek <codmonk21@gmail.com>
2023-12-02 13:15:49 +0530

    further changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { FaArrowRight } from 'react-icons/fa'
 import './App.css'
 import NavbarLeft from './components/header/NavbarLeft'
 import NavbarRight from './components/header/NavbarRight'
@@ -16,6 +17,45 @@ function App() {
       <main className={'main'}>
         <HeroContainer />
       </main>
+      {/* section */}
+      <div className="main">
+        <div className="container">
+          <h1 className="title h3">Millions run on Notion every day</h1>
+          <p className="subtitle p">
+            Powering the world’s best teams, from next-generation startups to established enterprises.
+          </p>
+          <button className="btn extra nobackgroundbtn">Read Customer Reviews <FaArrowRight /></button>
+          <img src="/main2.png" alt="hero image" className="hero-img" />
+        </div>
+      </div>
+
+      {/* section */}
+      <div className="main">
+        <div className="container">
+          <h1 className="title h3">Join a global movement. <br /> Unleash your creativity.</h1>
+          <p className="subtitle p">
+            Our vibrant community produces content, teaches courses, and leads events all over <br /> the world.
+          </p>
+          <button className="btn extra nobackgroundbtn">Learn more <FaArrowRight /></button>
+          <img src="/main3.png" alt="hero image" className="hero-img" />
+        </div>
+      </div>
+
+      {/* section */}
+      <div className="main">
+        <div className="container">
+          <h1 className="title">Get started for free</h1>
+          <p className="subtitle p">
+            lay around with it first. Pay and add your team later.
+          </p>
+          <div className='btn-container'>
+            <button className="btn">Try Notion free</button>
+            <button className="btn extra nobackgroundbtn">Request a demo <FaArrowRight /></button>
+          </div>
+          <img src="/main4.png" alt="hero image" className="hero-img" />
+        </div>
+      </div>
+
     </>
   )
 }
--- src/components/hero/CardContainer.jsx
+++ src/components/hero/CardContainer.jsx
@@ -1,15 +1,72 @@
+import { FaArrowRight } from "react-icons/fa";
 import { IoSparkles } from "react-icons/io5";
+import { GiBookAura } from "react-icons/gi";
+import { GoGoal } from "react-icons/go";
+import { IoDocumentTextOutline } from "react-icons/io5";
 const CardContainer = () => {
     return (
         <>
-            <div className="card">
-                <div className="card-header">
-                    <IoSparkles className="icon" />
-                    <h3 className="card-title">AI</h3>
-                </div>
-                <p className="card-description">Ask literally anyrhing Notion will answer</p>
-            </div>
 
+            <div className="card-container">
+                <div className="card">
+                    <div className="card-header">
+                        <IoSparkles className="icon" style={{
+                            color: '#9D34DA',
+                            background: '#F0E1F9'
+                        }} />
+                        <h3 className="card-title">AI</h3>
+                    </div>
+                    <p className="card-description">Ask literally anyrhing Notion will answer</p>
+                    <button style={{
+                        color: '#9D34DA',
+                    }} className="btn extra card-btn">Learn more <FaArrowRight /></button>
+                </div>
+
+                <div className="card">
+                    <div className="card-header" >
+                        <GiBookAura className="icon" style={{
+                            color: '#EA4E43',
+                            background: '#FCE9E8'
+                        }} />
+                        <h3 className="card-title">Wikis</h3>
+                    </div>
+                    <p className="card-description">Ask literally anyrhing Notion will answer</p>
+                    <button style={{
+                        color: '#EA4E43',
+                    }} className="btn extra card-btn">Learn more <FaArrowRight /></button>
+                </div>
+
+                <div className="card">
+                    <div className="card-header">
+                        <GoGoal className="icon" style={{
+                            color: '#0A85D1',
+                            background: '#E2ECF3'
+                        }} />
+                        <h3 className="card-title">Projects</h3>
+                    </div>
+                    <p className="card-description">Ask literally anyrhing Notion will answer</p>
+                    <button style={{
+                        color: '#0A85D1',
+                    }} className="btn extra card-btn">Learn more <FaArrowRight /></button>
+                </div>
+
+                <div className="card">
+                    <div className="card-header">
+                        <IoDocumentTextOutline className="icon" style={{
+                            color: '#FAA700',
+                            background: '#FFF6D6'
+                        }} />
+                        <h3 className="card-title">Docs</h3>
+                    </div>
+                    <p className="card-description">Ask literally anyrhing Notion will answer</p>
+                    <button style={{
+                        color: '#FAA700',
+                    }} className="btn extra card-btn">Learn more <FaArrowRight /></button>
+                </div>
+            </div>
+            <div className="image-container">
+                <img src="/main1.png" alt="caurosel-img" />
+            </div>
         </>
     )
 }
